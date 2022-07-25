735ec4a34f0ec0defb332a5b32bcae7f17848a16
sonhyeryeong <104577534+sonhyeryeong@users.noreply.github.com>
2022-07-26 00:18:51 +0900

    Add files via upload

---
+++ 220725_codiproject/src/LookCodiset.java
@@ -0,0 +1,136 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+/*
+ * -주요 컴포넌트 -
+ * userNamelbl: 해당 코디셋을 작성한 유저의 이름이 들어가는 라벨
+ * backBtn : [뒤로가기]버튼 . 메인페이지로 돌아간다.
+ * likeBtn : [좋아요] 버튼. 좋아요 테이블에 db가 전달되어야 함.
+ * getBtn[0-4] : [담기] 버튼 -> 누르면 해당 옷 db가 현재 사용자의 장바구니 db에 담긴다. 
+ * 
+ * */
+public class LookCodiset extends JFrame {
+	public LookCodiset(){
+		super("메인에서 게시글 클릭하면 보이는 화면");
+		
+		//제일 큰 프레임
+		JPanel pnl = new JPanel();
+		pnl.setLayout(null);
+		////////////top
+		JPanel topPnl = new JPanel();
+		topPnl.setBounds(0, 5, 834, 35);
+		topPnl.setLayout(null);//좌표값으로 위치를 정해주려면 기본 layout설정을 변경해주어야 하기 때문에 setLayout(null로 지정하여 주어야 한다)
+		
+		//유저이름 들어가는 라벨
+		JLabel userNamelbl = new JLabel("유저이름");
+		userNamelbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		userNamelbl.setLocation(543, 3);
+		userNamelbl.setSize(100, 25);
+		JLabel lbl = new JLabel("님의 코디셋");
+		lbl.setBounds(672, 5, 100, 25);
+		lbl.setHorizontalAlignment(SwingConstants.LEFT);
+		//이미지 아이콘
+		//[뒤로가기] 버튼
+		ImageIcon back = new ImageIcon(".\\img\\back.png");//이미지 경로지정
+		JButton backBtn = new JButton(back);
+		backBtn.setBounds(24, 2, 38, 27);
+		backBtn.setPreferredSize(new Dimension(32, 32));
+		backBtn.setBorderPainted(false);
+		backBtn.setBackground(Color.WHITE);;
+		//[좋아요]버튼
+		ImageIcon like = new ImageIcon(".\\img\\likeImg.png");//이미지 경로지정
+		JButton likeBtn = new JButton(like);
+		likeBtn.setBounds(784, 2, 38, 27);
+		likeBtn.setPreferredSize(new Dimension(32, 32));
+		likeBtn.setBorderPainted(false);
+		likeBtn.setBackground(Color.WHITE);
+		
+		topPnl.add(backBtn);
+		topPnl.add(userNamelbl);
+		topPnl.add(lbl);
+		topPnl.add(likeBtn);
+		/////////////
+		
+		
+		
+		// middle
+		JPanel middlePnl = new JPanel();
+		middlePnl.setBounds(0, 40, 834, 737);
+		middlePnl.setLayout(new GridLayout(0, 2));// 세로로 두 개로 나눔
+		//중간-왼쪽
+		JPanel leftPnlCodiset = new JPanel();
+		leftPnlCodiset.setBorder(new LineBorder(Color.pink,2));//영역 볼려고 테두리 설정해두었음
+		middlePnl.add(leftPnlCodiset);
+		
+		//중간- 오른쪽
+		JPanel rightPnl = new JPanel();
+		//5개의 구성요소가 있어야 함. 
+		//배열에다 구성요소 하나하나 담는다. 
+		List<JPanel> component = new ArrayList<JPanel>();
+		List<JLabel> photo = new ArrayList<JLabel>();
+		List<JLabel> text = new ArrayList<JLabel>();
+		List<JButton> getBtn = new ArrayList<JButton>();
+		
+		
+		for(int i =0; i<5;i++) {
+			JPanel component2 = new JPanel();
+			component.add(component2);
+			JLabel photo2 = new JLabel("/사진/ ");
+			photo.add(photo2);
+			JLabel text2 = new JLabel("정보가 들어가는 부분");
+			text.add(text2);
+			JButton getBtn2 = new JButton("담기");
+			getBtn.add(getBtn2);
+			getBtn.get(i).addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("눌렀지롱~~");
+					
+				}
+			});
+			component2.add(photo2);
+			component2.add(text2);
+			component2.add(getBtn2);
+			component2.setBorder(new LineBorder(Color.CYAN,2));
+			component.add(component2);
+			rightPnl.add(component2);
+		}
+		rightPnl.setLayout(new GridLayout(5,0));
+		
+		middlePnl.add(leftPnlCodiset);
+		middlePnl.add(rightPnl);
+		/////
+		
+		
+		
+		
+		
+		//구성요소 더하기
+		pnl.add(topPnl);
+		pnl.add(middlePnl);
+		
+		
+		getContentPane().add(pnl);
+		BoxLayout allLayout = new BoxLayout(pnl,BoxLayout.Y_AXIS);
+		setSize(850,850);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new LookCodiset().setVisible(true);
+	}
+}
+
---
+++ 220725_codiproject/src/MainFrame4.java
@@ -0,0 +1,186 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.LineBorder;
+
+public class MainFrame4 extends JFrame{
+	public MainFrame4() {
+		super("메인화면");
+		//제일 큰 프레임
+		JPanel pnl = new JPanel();
+		
+		//윗부분
+		JPanel topPnl = new JPanel();
+		JLabel lbl = new JLabel("메인화면 ");
+		JButton loginBtn  = new JButton("로그인");
+		JButton myBtn  = new JButton("내 정보");
+		topPnl.add(lbl);
+		topPnl.add(loginBtn);
+		topPnl.add(myBtn);
+		BoxLayout bltop = new BoxLayout(topPnl, BoxLayout.X_AXIS);// 입력패널 세로배치
+		topPnl.setLayout(bltop);
+		///////////////
+		
+		//중간부분
+		JPanel middlePnl = new JPanel();//중간 전체 감싸는 패널
+		//중간 왼쪽 큰 게시글 하나
+		JPanel leftPnl = new JPanel();
+		JLabel lbl1 = new JLabel("큰 게시글");
+		leftPnl.setBorder(new LineBorder(Color.CYAN,2));
+		leftPnl.add(lbl1);
+		
+		
+		
+		//중간 오른쪽 게시글 6개 
+		JPanel rightPnl = new JPanel();
+		rightPnl.setLayout(new GridLayout(2,0));
+		//top3 부분
+		JPanel top3Pnl = new JPanel();
+		JLabel toplbl1 = new JLabel("top1");
+		JLabel toplbl2 = new JLabel("top2");
+		JLabel toplbl3 = new JLabel("top3");
+		toplbl1.setBorder(new LineBorder(Color.CYAN,2));
+		toplbl2.setBorder(new LineBorder(Color.CYAN,2));
+		toplbl3.setBorder(new LineBorder(Color.CYAN,2));
+		top3Pnl.setLayout(new GridLayout(0,3));
+		top3Pnl.add(toplbl1);
+		top3Pnl.add(toplbl2);
+		top3Pnl.add(toplbl3);
+		rightPnl.add(top3Pnl);
+		
+		//신상 부분
+		JPanel pnlNew = new JPanel();
+		JLabel lblnew1 = new JLabel("신상1");
+		JLabel lblnew2 = new JLabel("신상2");
+		JLabel lblnew3 = new JLabel("신상3");
+		lblnew1.setBorder(new LineBorder(Color.CYAN,2));
+		lblnew2.setBorder(new LineBorder(Color.CYAN,2));
+		lblnew3.setBorder(new LineBorder(Color.CYAN,2));
+		pnlNew.setLayout(new GridLayout(0,3));
+		pnlNew.add(lblnew1);
+		pnlNew.add(lblnew2);
+		pnlNew.add(lblnew3);
+		rightPnl.add(pnlNew);
+		//중간패널 끝
+		
+		
+		middlePnl.add(leftPnl);
+		middlePnl.add(rightPnl);
+		middlePnl.setLayout(new GridLayout(0,2));
+		////////중간 부분 끝
+		
+		
+		
+		//코디셋 추천하는 왼쪽섹션
+//		
+		
+		//탭으로 넘기는 오른쪽 섹션
+//		JTabbedPane  tapPnl = new JTabbedPane();
+		
+		//top3 부분
+//		JPanel top3Pnl = new JPanel();
+//		top3Pnl.setBackground(Color.PINK);
+		
+//		top3Pnl.setLayout(new GridLayout(3,0));
+//		top3Pnl.add(toplbl1);
+//		top3Pnl.add(toplbl2);
+//		top3Pnl.add(toplbl3);
+		
+//		tapPnl.add(top3Pnl);
+//		tapPnl.add("TOP3",top3Pnl);
+		
+		//신상 부분
+//		JPanel pnlNew = new JPanel();
+//		JLabel lblnew1 = new JLabel("신상1");
+//		JLabel lblnew2 = new JLabel("신상2");
+//		JLabel lblnew3 = new JLabel("신상3");
+//		pnlNew.setLayout(new GridLayout(3,0));
+//		pnlNew.add(lblnew1);
+//		pnlNew.add(lblnew2);
+//		pnlNew.add(lblnew3);
+		
+//		tapPnl.add(pnlNew);
+//		tapPnl.add("신제품",pnlNew);
+		//중간패널 끝
+		
+		
+		
+		
+		
+		
+		
+		
+		// bottom
+		JPanel bottomPnl = new JPanel();//bottom전체 감싸는 큰 패널
+		JButton writeBtn = new JButton("글쓰기");
+		bottomPnl.add(writeBtn);
+		BoxLayout blbottom = new BoxLayout(bottomPnl, BoxLayout.Y_AXIS);// 입력패널 세로배치
+		bottomPnl.setLayout(blbottom);
+		//
+		
+		//구성요소 더하기
+//		pnl.add(topPnl);
+//		GroupLayout gl_topPnl = new GroupLayout(topPnl);
+//		gl_topPnl.setHorizontalGroup(
+//			gl_topPnl.createParallelGroup(Alignment.LEADING)
+//				.addGroup(gl_topPnl.createSequentialGroup()
+//					.addGap(140)
+//					.addComponent(lbl)
+//					.addGap(5)
+//					.addComponent(loginBtn)
+//					.addGap(5)
+//					.addComponent(myBtn))
+//		);
+//		gl_topPnl.setVerticalGroup(
+//			gl_topPnl.createParallelGroup(Alignment.LEADING)
+//				.addGroup(gl_topPnl.createSequentialGroup()
+//					.addGap(9)
+//					.addComponent(lbl))
+//				.addGroup(gl_topPnl.createSequentialGroup()
+//					.addGap(5)
+//					.addComponent(loginBtn))
+//				.addGroup(gl_topPnl.createSequentialGroup()
+//					.addGap(5)
+//					.addComponent(myBtn))
+//		);
+//		topPnl.setLayout(gl_topPnl);
+//		middlePnl.setLayout(new BoxLayout(middlePnl, BoxLayout.X_AXIS));
+//		mainCodiPnl.setLayout(new GridLayout(0, 1, 0, 0));
+		//		lbl1.setBounds(12, 5, 179, 83);
+				
+//		middlePnl.add(mainCodiPnl);
+//		mainCodiPnl.setLayout(new GridLayout(0, 1, 0, 0));
+//		mainCodiPnl.add(lbl1);
+//		mainCodiPnl.add(lbl2);
+//		mainCodiPnl.add(lbl3);
+//		middlePnl.add(tapPnl);
+		
+		
+		
+		pnl.add(topPnl);
+		pnl.add(middlePnl);
+		pnl.add(bottomPnl);
+		BoxLayout allLayout = new BoxLayout(pnl,BoxLayout.Y_AXIS);
+		pnl.setLayout(allLayout);
+		
+		
+		getContentPane().add(pnl);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(850,850);
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		new MainFrame4().setVisible(true);
+	}
+}
